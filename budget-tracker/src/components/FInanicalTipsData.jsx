a30cc3c90e615baa83e2cb1613489e1fd78309ac
Mosobe Lulu <mosobelulu@gmail.com>
2024-04-22 21:02:59 +0200

    wip local storage

---
+++ budget-tracker/src/components/FInanicalTipsData.jsx
@@ -0,0 +1,22 @@
+export const FinancialTipsData = [
+    {
+        imageUrl: "https://i.pinimg.com/564x/25/fd/19/25fd19bebed00b14074eb68ec50aaa11.jpg",
+        description: "Create a monthly budget to track your income and expenses."
+    },
+    {
+        imageUrl: "https://i.pinimg.com/564x/6f/31/a9/6f31a91ff90081766f3593f052ea3802.jpg",
+        description: "Save for major expenses like holidays and home repairs to avoid debt."
+    },
+    {
+        imageUrl: "https://cdn.breadfast.com/wp-content/uploads/2021/12/shopping-mistakes-1024x511.jpg",
+        description: "Avoid impulse buying by making a shopping list before you go to the store."
+    },
+    {
+        imageUrl: "https://econologicsfinancialadvisors.com/wp-content/uploads/2019/07/celebrate-finances.jpg",
+        description: "Celebrate your financial milestones to stay motivated in your financial journey."
+    },
+    {
+        imageUrl: "https://www.ci-associates.com/wp-content/uploads/2023/01/income.jpg",
+        description: "Diversifying your income streams can help to increase your earning potential and help you build wealth faster. By having multiple streams of income, you can reduce your financial risks and make it easier to reach your financial goals."
+    }
+]
--- budget-tracker/src/pages/FinancialTips.jsx
+++ budget-tracker/src/pages/FinancialTips.jsx
@@ -1,31 +1,10 @@
 import { useEffect, useState } from 'react';
 import classes from './FinancialTips.module.css';
+import { FinancialTipsData } from '../components/FInanicalTipsData';
 
 function FinancialTips() {
   const [data, setData] = useState([]);
-  const databaseURL = 'https://budget-pal-2db6a-default-rtdb.firebaseio.com/';
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch(databaseURL);
-
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-
-        const jsonData = await response.json();
-
-        // Convert data object to an array or handle it as needed
-        const dataArray = Object.values(jsonData);
-        setData(dataArray);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    };
-
-    fetchData();
-  }, []);
+ 
 
   if (!data) {
     return <p>Loading...</p>;
@@ -35,10 +14,10 @@ function FinancialTips() {
     <div className={classes.finTips}>
       <h1 className={classes.title}>Smart Money Moves: Your Guide to Financial Well-being</h1>
       <div className={classes.tipContainer}>
-        {data.map((tipData, index) => (
+        {FinancialTipsData.map((item, index) => (
           <div key={index} className={classes.tipCard}>
-            <img src={tipData.image} alt={`Tip ${index + 1} Image`} className={classes.image} />
-            <p className={classes.description}>{tipData.description}</p>
+            <img src={item.imageUrl} alt={`Tip ${index + 1} Image`} className={classes.image} />
+            <p className={classes.description}>{item.description}</p>
           </div>
         ))}
       </div>
